84a9d59b1529d9761bd7d8c144f74cc964d9e226
Stasia Hung <stasia_hung@tomofun.com>
2025-01-15 22:24:29 +0800

    init app

---
+++ easy-voyager/EasyVoyager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyVoyager
+//
+//  Created by Stasia Hung on 2025/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ easy-voyager/EasyVoyager/EasyVoyagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyVoyagerApp.swift
+//  EasyVoyager
+//
+//  Created by Stasia Hung on 2025/1/15.
+//
+
+import SwiftUI
+
+@main
+struct EasyVoyagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
